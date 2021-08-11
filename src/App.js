510ecbd6c0dff250a2dfe32a28904c4f04143881
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-08-11 09:07:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Card from './Card';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./Card";
 import { data } from "./data";
-import Filter from './filter';
-import { useEffect, useState } from 'react';
+import Filter from "./filter";
+import { useEffect, useState } from "react";
 function App() {
-  console.log(data)
+  console.log(data);
   let carData = data;
 
-  const [filteredData,setFilterData] = useState([]);
-  const [pageCount,setPageCount] = useState(1);
-  const [pageNumbers,setPageNumber] = useState([]);
-  const [currentPage,setCurrentPage] = useState(1);
-  
+  const [filteredData, setFilterData] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [pageNumbers, setPageNumber] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
   /*
     {
       pageNum : 3
     }
   */
-  let filter = (filterConfig) => {
-      // filter the whole data and return result 
-      let productObj = [];
-      for (let index = 0; index < 6; index++) {
-        productObj.push(carData[((filterConfig - 1) * 6) + index]);
-      }
+  let filter = filterConfig => {
+    // filter the whole data and return result
+    let productObj = [];
+    for (let index = 0; index < 6; index++) {
+      productObj.push(carData[(filterConfig - 1) * 6 + index]);
+    }
 
-      setFilterData([...productObj]);
-  }
-
-
+    setFilterData([...productObj]);
+  };
 
   useEffect(() => {
     // Will execute for one time during component render in DOM
-    console.log("One time")
-    
-    
-    let count = Math.ceil(data.length / 6)
-    setPageCount(count)
+    console.log("One time");
+
+    let count = Math.ceil(data.length / 6);
+    setPageCount(count);
     for (let index = 1; index < count; index++) {
-      pageNumbers.push(index)
+      pageNumbers.push(index);
     }
     filter(currentPage);
-  },[])
-
+  }, []);
 
   return (
     <div className="container">
@@ -53,27 +49,40 @@ function App() {
         </div>
       </div>
       <div className="row mt-3">
-        <Filter/>
+        <Filter />
 
         <div className="col-lg-9">
           <div className="row">
-            {
-              filteredData.map((carObj) => {
-                  return <Card carObj={carObj}/>
-              })
-            }
+            {filteredData.map(carObj => {
+              return <Card carObj={carObj} />;
+            })}
           </div>
           <div className="row mt-2">
             <div className="col-lg-12">
               <nav aria-label="Page navigation example">
                 <ul class="pagination">
-                  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                  {
-                    pageNumbers.map((number) => {
-                      return <li class="page-item"><button onClick={() => filter(number)} class="page-link">{number}</button></li>
-                    })
-                  }
-                  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">
+                      Previous
+                    </a>
+                  </li>
+                  {pageNumbers.map(number => {
+                    return (
+                      <li class="page-item">
+                        <button
+                          onClick={() => filter(number)}
+                          class="page-link"
+                        >
+                          {number}
+                        </button>
+                      </li>
+                    );
+                  })}
+                  <li class="page-item">
+                    <a class="page-link" href="#">
+                      Next
+                    </a>
+                  </li>
                 </ul>
               </nav>
             </div>
